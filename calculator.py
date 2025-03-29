8c47447e8cb3fb71bdba8dc2efcc1aee21c78470
Pankaj Kumar <pankaj30.s@gmail.com>
2025-03-29 15:54:55 +0800

    Addition added by pankaj

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# write a sample calculator (Just arthemetic operations) code.
+
+num_one = 40
+num_two = 20
+
+# Addition for Pankaj
+print ("Addition: ", num_one + num_two)
+
+
+# Multiplication for Rohit
+
+
+
+# Subtraction for Sulano
+
+
+
+# Division for Rocky
+
+
+
+# Square root for Ehsan
+
